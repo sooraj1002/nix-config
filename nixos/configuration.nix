069e1db47360be6f7b7ad42ce7df1a3d0071a919
sooraj1002 <ksooraj162@gmail.com>
2023-12-26 17:23:52 +0530

    added bluetooth

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,6 +15,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
+    ./bluetooth.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
